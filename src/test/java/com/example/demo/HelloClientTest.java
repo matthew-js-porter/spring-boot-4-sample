b95e9ef623d6e9bb47073686811d14a17622ec1e
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-09-10 20:46:26 -0400

    updating HelloClientTest to use @RestClientTest. This requires a workaround of adding HttpServiceClientAutoConfiguration to AutoConfigureWebClient. I will open an issue to have this done at the framework leve.

--- src/test/java/com/example/demo/HelloClientTest.java
+++ src/test/java/com/example/demo/HelloClientTest.java
@@ -2,19 +2,27 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestToUriTemplate;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RestClientTest(HelloClientTest.class)
 class HelloClientTest {
 
     @Autowired
     private HelloClient helloClient;
 
+    @Autowired
+    MockRestServiceServer mockRestServiceServer;
+
     @Test
     void hello() {
+        mockRestServiceServer.expect(requestToUriTemplate("http://localhost:8080/hello?version={version}", 2))
+                .andRespond(withSuccess("Hello There!", MediaType.TEXT_PLAIN));
         assertThat(helloClient.hello()).isEqualTo("Hello There!");
     }
-
 }
